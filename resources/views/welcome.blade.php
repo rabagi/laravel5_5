9edf32d22e3d5d58146f0dbfdaff48e02697b3ec
rabagi <programador1@telecomo.com.mx>
2017-10-10 17:12:24 -0500

    helpers nuevos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome! {{$name}}
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/',  'welcome', ['name' => 'RBG']);
+
+Route::redirect('welcome', '/');
